46bd2bd8c8171296351bc61a61393b6c5db581e1
Tyler Garlick <tgarlick@heliotraining.com>
2018-10-30 10:19:47 -0600

    Added a new form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
+  
+  state = {
+    name: '',
+    email: '',
+    password: '',
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          
+          console.log(this.state)
+          
+          
+        }}
+      >
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+            minLength="2"
+            maxLength="128"
+            onChange={e => this.setState({ name: e.target.value })}
+            value={this.state.name}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            required
+            onChange={e => this.setState({ email: e.target.value })}
+            value={this.state.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            minLength="7"
+            maxLength="128"
+            required
+            onChange={e => this.setState({ password: e.target.value })}
+            value={this.state.password}
+          />
+        </div>
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
+    )
   }
 }
 
-export default App;
+export default App
